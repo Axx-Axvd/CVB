bd1a8178da5c92378d3426ed1af8596805442607
AlexeyRybaklin <rybalkin@sfedu.ru>
2024-11-14 19:04:34 +0300

    swagger

--- CVB/Program.cs
+++ CVB/Program.cs
@@ -1,23 +1,41 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
+// Добавляем службы к контейнеру
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+// Добавляем Swagger
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "Reminder API",
+        Description = "API для управления напоминаниями",
+        Contact = new OpenApiContact
+        {
+            Name = "Ваше Имя",
+            Email = "example@example.com"
+        }
+    });
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+// Настройка Swagger только для режима разработки
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Reminder API v1");
+        c.RoutePrefix = string.Empty; // Чтобы Swagger открывался по корневому URL
+    });
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
 
 app.MapControllers();
